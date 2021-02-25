585bc509c0c039663a18920e919f3ba0408299c6
hamza15 <sardan5@vt.edu>
2021-02-25 10:45:56 -0500

    commit tree 1d22fecd3227c3cb55f9f7e0464e2c5e5ecd9275

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(action="roller-skate") {
+    return `This Saturday, I want to ${action}!`
+}
+
+function mondayWork(action="go to the office") {
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(param1="*") {
+    return function (param2="special") {
+        return `You are ${param1}${param2}${param1}!`
+    };
+}
+
+let Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    multiply: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(start, arr) {
+    let int = start;
+    for (let fn of arr) {
+        int = fn(int)
+    }
+    return int;
+}
